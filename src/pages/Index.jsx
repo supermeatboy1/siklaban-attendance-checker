104176ec8d44f4301256863c50a03e4c3fed8faa
supermeatboy1 <124642134+supermeatboy1@users.noreply.github.com>
2025-04-10 05:55:32 +0800

    Implement manual attendance entry

---
+++ src/pages/Index.jsx
@@ -0,0 +1,196 @@
+import { useNavigate } from "react-router-dom";
+
+import { useState, useEffect, useRef } from 'react'
+import intramuralsWordmark from '../assets/Wordmark.png'
+
+import Button from '../components/Button.jsx';
+import ConfirmationModal from "../components/ConfirmationModal";
+import ErrorDialogModal from "../components/ErrorDialogModal";
+import LoadingModal from "../components/LoadingModal";
+import DialogModal from "../components/DialogModal";
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_NEXT_PUBLIC_SUPABASE_ANON_KEY)
+
+const Index = () => {
+  const navigate = useNavigate();
+
+  const [clockIn, setClockIn] = useState(true)
+  const [useRFID, setUseRFID] = useState(true)
+  const [idInput, setIdInput] = useState("")
+  const [lastInputTime, setLastInputTime] = useState(Date.now())
+  const [errorLog, setErrorLog] = useState(null);
+  const [idConfirm, setIdConfirm] = useState(null);
+  const [idConfirmModal, setIdConfirmModal] = useState(false);
+  const [loadingModal, setLoadingModal] = useState(null);
+  const [successModal, setSuccessModal] = useState(false);
+  const [foundStudent, setFoundStudent] = useState("[ERROR]");
+  const idInputRef = useRef(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (useRFID && document.activeElement != idInputRef.current && !idConfirmModal) {
+        console.log("Bringing back the focus...")
+        idInputRef.current?.focus();
+      }
+    }, 200);
+
+    return () => clearInterval(interval);
+  }, [useRFID, idConfirm, idInputRef])
+
+  const fetchStudent = async (isRFID, currentInput) => {
+    if (currentInput === undefined || currentInput === null || currentInput === "") {
+      setLoadingModal(null);
+      return;
+    }
+
+    console.log(`isRFID: ${isRFID}, currentInput: ${currentInput}`)
+
+    if (!isRFID) {
+      const { data, error } = await supabase
+        .from('StudentsWithCluster')
+        .select("student_id, name, cluster_name")
+        .eq("student_id", currentInput)
+
+      setLoadingModal(null);
+      if (error) {
+        setErrorLog(error["code"] + " - " + error["message"]);
+        return;
+      }
+      if (data.length == 0) {
+        setErrorLog(`Student with ID "${currentInput}" not found.`);
+        return;
+      } else {
+        setIdConfirm(data[0]);
+        setIdConfirmModal(true);
+
+        console.log(data[0]);
+      }
+    } else {
+      const { data, error } = await supabase
+        .from('RfidToStudent')
+        .select("student_id, rfid")
+        .eq("rfid", currentInput)
+
+      setLoadingModal(null);
+      if (error) {
+        setErrorLog(error["code"] + " - " + error["message"]);
+        return;
+      }
+      if (data.length == 0) {
+        setErrorLog(`RFID "${currentInput}" not found.`);
+      } else {
+        fetchStudent(false, data[0]["student_id"]);
+      }
+    }
+  }
+
+  const recordAttendance = async (studentId) => {
+    const attendanceType = clockIn ? "IN" : "OUT";
+    console.log(`Recording ${attendanceType} attendance for ${studentId}`);
+
+    setIdConfirm(null);
+    setIdConfirmModal(false);
+
+    setLoadingModal("Recording attendance...");
+
+    const { error } = await supabase
+      .from('Attendance')
+      .insert({ student_id: studentId, attendance_type: attendanceType });
+
+    setLoadingModal(null);
+
+    if (error) {
+      setErrorLog(error);
+    } else {
+      setSuccessModal(true);
+    }
+  }
+
+  return (
+    <>
+      <div className="flex flex-col">
+        <div className="m-auto">
+          <div className="w-5/8 m-auto">
+            <img src={intramuralsWordmark} alt="Siklaban 2025 Wordmark" className="pt-10 pb-5" />
+          </div>
+          <div className="flex flex-row justify-center pb-16">
+            <h1 className="text-yellow-300 text-4xl">Attendance Checker</h1>
+          </div>
+          <div className="flex flex-row justify-center">
+            <Button type="button" onClick={() => { setUseRFID(true); setIdInput("") }} selected={useRFID}>Use RFID</Button>
+            <Button type="button" onClick={() => { setUseRFID(false); setIdInput("") }} selected={!useRFID}>Use Student ID</Button>
+            <Button type="button" onClick={() => { navigate("/manual_attendance") }}>Manual Attendance</Button>
+          </div>
+          <div className="p-10">
+            <label className="block text-sm font-medium text-gray-100">{useRFID ? "RFID Input" : "Student ID Input"}</label>
+            <input
+                ref={idInputRef}
+                type='text'
+                name='idInput'
+                className="mt-1 block w-full p-2 border border-gray-300 bg-white rounded-md"
+                value={idInput}
+                onChange={(e) => {
+                  const { name, value } = e.target;
+                  setIdInput(value);
+                }}
+                onKeyDown={(e) => {
+                  if (e.key == 'Enter') {
+                    console.log("Current input: " + idInput);
+                    const currentIdInput = idInput;
+
+                    setLoadingModal("Looking for student information...");
+                    fetchStudent(useRFID, currentIdInput);
+                    setIdInput("");
+                  } else if (useRFID) {
+                    const diff = Date.now() - lastInputTime;
+                    setLastInputTime(Date.now());
+                    if (diff > 250) {
+                      setIdInput("");
+                    }
+                  }
+                }}
+            />
+          </div>
+          <div className="flex flex-row justify-center">
+            <label className="text-sm font-medium text-gray-100 self-center pr-8">Attendance Type</label>
+            <Button type="button" onClick={() => { setClockIn(true) }} selected={clockIn}>IN</Button>
+            <Button type="button" onClick={() => { setClockIn(false) }} selected={!clockIn}>OUT</Button>
+          </div>
+        </div>
+      </div>
+        {(idConfirm != null && idConfirmModal) && (
+            <ConfirmationModal
+                noButton='No'
+                yesButton='Yes'
+                message={'Are you ' + idConfirm["name"] + " from the cluster " + idConfirm["cluster_name"] + "?"}
+                onYes={() => {setIdConfirmModal(false); recordAttendance(idConfirm["student_id"]) } }
+                onNo={() => {setIdConfirmModal(false); setIdConfirm(null);} }
+            />
+        )}
+        {errorLog != null && (
+            <ErrorDialogModal
+                buttonText='Ok'
+                message='An error occured. Please check the error message below.' 
+                errorLog={errorLog}
+                onClick={() => setErrorLog(null) }
+            />
+        )}
+        {successModal && (
+            <DialogModal
+                buttonText='Ok'
+                message='Attendance recorded.' 
+                onClick={() => setSuccessModal(false) }
+            />
+        )}
+        {loadingModal != null && (
+            <LoadingModal
+                message={loadingModal}
+            />
+        )}
+    </>
+  )
+}
+
+export default Index
---
+++ src/pages/ManualAttendance.jsx
@@ -0,0 +1,129 @@
+import { useNavigate } from "react-router-dom";
+
+import { useState, useEffect, useRef } from 'react'
+import intramuralsWordmark from '../assets/Wordmark.png'
+
+import Button from '../components/Button.jsx';
+import LoadingModal from "../components/LoadingModal";
+import DialogModal from "../components/DialogModal";
+import ErrorDialogModal from "../components/ErrorDialogModal";
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_NEXT_PUBLIC_SUPABASE_ANON_KEY)
+
+const ManualAttendance = () => {
+  const navigate = useNavigate();
+
+  const CLUSTERS = ["ATYCB - Ardor", "CHS - Fuego", "CAS - Incendio", "CEA - Lumbre", "CCIS - Stella"]
+
+  const [clockIn, setClockIn] = useState(true)
+  const [formData, setFormData] = useState({"studentId": "", "studentName": "", "cluster": 1})
+  const [loadingModal, setLoadingModal] = useState(null);
+  const [errorLog, setErrorLog] = useState(null);
+  const [successModal, setSuccessModal] = useState(false);
+
+  const changeForm = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  }
+
+  const recordAttendance = async (e) => {
+    const attendanceType = clockIn ? "IN" : "OUT";
+
+    e.preventDefault();
+    console.log(formData);
+
+    setLoadingModal("Recording manual attendance...");
+
+    const { error } = await supabase
+      .from('ManualAttendance')
+      .insert({ student_id: formData["studentId"], type: attendanceType,
+                name: formData["studentName"], cluster_id: formData["cluster"] });
+
+    setLoadingModal(null);
+
+    if (error) {
+      setErrorLog(error["code"] + " - " + error["message"]);
+    } else {
+      setSuccessModal(true);
+    }
+  }
+
+  return (
+    <>
+      <form className="flex flex-col" onSubmit={recordAttendance}>
+        <div className="m-auto">
+          <div className="w-5/8 m-auto">
+            <img src={intramuralsWordmark} alt="Siklaban 2025 Wordmark" className="pt-10 pb-5" />
+          </div>
+          <div className="flex flex-row justify-center">
+          	<h1 className="text-yellow-300 text-4xl">Manual Attendance</h1>
+          </div>
+          <div className="flex flex-row justify-start px-10 pt-4">
+            <Button type="button" onClick={() => { navigate("/") }} selected={true}>Go Back</Button>
+          </div>
+          <div className="p-10">
+            <label className="block text-sm font-medium text-gray-100">Student Name</label>
+            <input
+                type='text'
+                name='studentName'
+                className="mt-1 block w-full p-2 border border-gray-300 bg-white rounded-md"
+                value={formData["studentName"]}
+                onChange={changeForm}
+                required
+            />
+            <label className="block text-sm font-medium text-gray-100 pt-4">Student ID</label>
+            <input
+                type='text'
+                name='studentId'
+                className="mt-1 block w-full p-2 border border-gray-300 bg-white rounded-md"
+                value={formData["studentId"]}
+                onChange={changeForm}
+                required
+            />
+            <label className="block text-sm font-medium text-gray-100 pt-4">Cluster</label>
+            <select className="mt-1 block w-full p-2.5 border border-gray-300 bg-white rounded-md" name='cluster' value={formData["cluster"]} onChange={changeForm}>
+                {CLUSTERS.map((row, rowIndex) => (
+                      <option value={rowIndex + 1}>{row}</option>
+                    )
+                )}
+            </select>
+            <label className="block text-sm font-medium text-gray-100 py-4">Attendance Type</label>
+            <div className="flex flex-row justify-start">
+              <Button type="button" onClick={() => { setClockIn(true) }} selected={clockIn}>IN</Button>
+              <Button type="button" onClick={() => { setClockIn(false) }} selected={!clockIn}>OUT</Button>
+              <div className="flex-grow"></div> 
+              <Button type="submit" selected={true}>Record Attendance</Button>
+            </div>
+          </div>
+        </div>
+      </form>
+      {errorLog != null && (
+          <ErrorDialogModal
+              buttonText='Ok'
+              message='An error occured. Please check the error message below.' 
+              errorLog={errorLog}
+              onClick={() => setErrorLog(null) }
+          />
+      )}
+      {successModal && (
+          <DialogModal
+              buttonText='Ok'
+              message='Attendance recorded.' 
+              onClick={() => setSuccessModal(false) }
+          />
+      )}
+      {loadingModal != null && (
+          <LoadingModal
+              message={loadingModal}
+          />
+      )}
+    </>
+  )
+}
+
+export default ManualAttendance
