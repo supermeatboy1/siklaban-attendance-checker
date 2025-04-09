ff5aa9e407b66d3c49ea135178f6368abd2f9895
supermeatboy1 <124642134+supermeatboy1@users.noreply.github.com>
2025-04-10 02:13:17 +0800

    Create initial landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import intramuralsLogo from './assets/Logo.png'
+import intramuralsWordmark from './assets/Wordmark.png'
+
+import Button from './components/Button.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
+  const [clockIn, setClockIn] = useState(true)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-zinc-950 flex flex-col h-screen">
+        <div className="w-4/10 mx-auto">
+          <img src={intramuralsWordmark} alt="Siklaban 2025 Wordmark" className="p-10" />
+        </div>
+        <div className="mx-auto">
+          <Button type="button">IN</Button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({ children, onClick, type, selected }) => {
+    return (
+        <button type={type} className={"flex justify-center items-center gap-2 text-white bg-orange-400/70 hover:bg-orange-400/90 font-bold rounded-lg text-sm px-5 py-2.5 me-2 mb-2 cursor-pointer"} onClick={onClick}>
+            {children}
+        </button>
+    );
+}
+
+export default Button;
